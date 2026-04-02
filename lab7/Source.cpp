3367c078acbbcc9dd9bb3fc1f52e38eb6f0f1a66
derishabl <artemtop007228@gmail.com>
2026-04-02 16:10:34 +0300

    Add files lab7

---
+++ lab7/Source.cpp
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS strtok
+#include <iostream>
+#include <Windows.h>
+
+void main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    char text[] = "assasbsa gasgag ab a fggd g";
+    char* word;
+    int i = 1;
+
+    printf("Текст: %s\n", text);
+    printf("Слова, начинающиеся на 'а':\n");
+
+    word = strtok(text, " .,!?;:");
+
+    while (word != NULL) {
+        if (tolower((char)word[0]) == 'a') {
+            printf("%d. %s\n", i, word);
+            i++;
+        }
+        word = strtok(NULL, " .,!?;:");
+    }
+}
